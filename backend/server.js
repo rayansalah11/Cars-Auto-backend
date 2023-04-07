5dba791a984756a2cd32d9dab663a4c34232d295
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-07 14:39:56 +0300

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
 const app = require("./app");
+const dotenv = require("dotenv");
+
+
+// config
+if(process.env.NODE_ENV!=="PRODUCTION"){
+    require("dotenv").config({
+        path:"backend/config/.env"
+    })}
+    
+// create server
+const server = app.listen(process.env.PORT,() =>{
+    console.log(`Server is working on http://localhost:${process.env.PORT}`)
+})
