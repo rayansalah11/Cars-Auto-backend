619b8d73741c1a521d84ff675466804fbdbc9245
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-08 20:33:08 +0300

    commit

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 
 
+app.use(express.json());
 
-
+// route import 
+const product = require("./routes/ProductRoute");
+app.use("/api/v2", product);
 module.exports = app
---
+++ backend/controller/ProductController.js
@@ -0,0 +1,7 @@
+
+exports.getAllProducts =(req ,res) =>{
+    res.status(200).json({
+        massage:"route is working fine"
+    })}
+
+
