79e8b37f2f7d109c9d10d13d87241f8dbb1fb036
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-11 16:46:41 +0300

    commit

--- backend/models/ProductModel.js
+++ backend/models/ProductModel.js
@@ -1,5 +1,10 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
- 
+    name:{
+        type:String,
+        required:[true, "Please enter a name of a product"],
+        trim: true,
+        maxLength:[20, "Product name not exceed than 20 characters"]
+    }]
 module.exports = mongoose.model("Product",productSchema);
