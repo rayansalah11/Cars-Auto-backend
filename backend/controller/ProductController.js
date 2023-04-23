d10bc058b39fa4af4b97891ce8c31eddbf7b42e8
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-23 17:57:59 +0300

    get All Products

--- backend/controller/ProductController.js
+++ backend/controller/ProductController.js
@@ -33,12 +33,31 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
     product,
   });
 });
-// Get All Product (Admin)
+// Get All Product (Admin
 exports.getAdminProducts = catchAsyncErrors(async (req, res, next) => {
   const products = await Product.find();
 
   res.status(200).json({
     success: true,
     products,
+  });
+});
+
+// get All Products
+exports.getAllProducts = catchAsyncErrors(async (req, res) => {
+  const resultPerPage = 8;
+
+  const productsCount = await Product.countDocuments();
+
+  const feature = new Features(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage);
+  const products = await feature.query;
+  res.status(200).json({
+    success: true,
+    products,
+    productsCount,
+    resultPerPage,
   });
 });
