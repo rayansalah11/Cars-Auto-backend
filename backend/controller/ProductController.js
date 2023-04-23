8ff229c41a7dbd6e3c3be2d4fca2ba7240f805f9
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-23 16:34:46 +0300

    delete

--- backend/controller/ProductController.js
+++ backend/controller/ProductController.js
@@ -26,18 +26,26 @@ exports.updateProduct = (async (req, res, next) => {
     product,
   });
 });
-//delet product
-exports.deleteProduct = (async (req, res, next) => {
-
+// delete Product
+exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
 
   if (!product) {
-    return next(new ErrorHandler("Product not found", 404));
+    return next(new ErrorHandler("Product is not found with this id", 404));
   }
+
+  // Deleting images from cloudinary
+  for (let i = 0; 1 < product.images.length; i++) {
+    const result = await cloudinary.v2.uploader.destroy(
+      product.images[i].public_id
+    );
+  }
+
   await product.remove();
-  
+
   res.status(200).json({
     success: true,
-    message: "Product is deleted successfully",
+    message: "Product deleted succesfully",
   });
 });
+
