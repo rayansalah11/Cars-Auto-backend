69215cbc12b439f447d15246f04101488ff8f99c
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-24 22:20:34 +0300

    commit

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -9,3 +9,9 @@ module.exports = (err, req, res, next) => {
     const message = `Resources not found with this id.. Invalid ${err.path}`;
     err = new ErrorHandler(message, 400);
   }
+  
+  // Duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate key ${Object.keys(err.keyValue)} Entered`;
+    err = new ErrorHandler(message, 400);
+  }
