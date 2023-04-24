e1de8d9cff8597113775d30072f741ce85c5a954
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-24 22:19:11 +0300

    commit

---
+++ backend/middleware/error.js
@@ -0,0 +1,5 @@
+const ErrorHandler = require("../utils/ErrorHandler");
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal server Error";
