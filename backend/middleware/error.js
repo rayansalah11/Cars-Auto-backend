93ba7ae9e29cbe714af7261bf557db80173cb985
RAYAN SALAH <rayan.salah11@gmail.com>
2023-04-29 17:29:53 +0300

    commit

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -9,19 +9,19 @@ module.exports = (err, req, res, next) => {
     const message = `Resources not found with this id.. Invalid ${err.path}`;
     err = new ErrorHandler(message, 400);
   }
-  
+
   // Duplicate key error
   if (err.code === 11000) {
     const message = `Duplicate key ${Object.keys(err.keyValue)} Entered`;
     err = new ErrorHandler(message, 400);
   }
-  
+
   // wrong jwt error
   if (err.name === "JsonWebTokenError") {
     const message = `Your url is invalid please try again letter`;
     err = new ErrorHandler(message, 400);
   }
-  
+
   // jwt expired
   if (err.name === "TokenExpiredError") {
     const message = `Your Url is expired please try again letter!`;
